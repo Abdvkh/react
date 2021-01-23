a194f49666071d869179c3dbefeeda96a85cb3fa
abdvkh <e-address@bk.ru>
2021-01-23 11:45:12 +0500

    Basic intro/setup; Basics of JSX;

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Greeting(){
+    return (
+        <div>
+            <h1>Hello world! This is my first component</h1>
+        </div>
+    );
+}
+
+// const Greeting = () => {
+//     return React.createElement(
+//         'div',
+//         {},
+//         React.createElement(
+//             'h1',
+//             {},
+//             'Hello world')
+//     );
+// }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Greeting/>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
