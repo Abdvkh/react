a0a9791c2fc489ace0991156ae83c0d4ca5ec448
abdvkh <e-address@bk.ru>
2021-01-23 12:13:02 +0500

    Nested components; React devtools;

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/** JSX rules
- * return single element
- * div / section / article or Fragment
- * use camelCase for html attribute
- * className instead of class
- * close every element
- * formatting
+/** Nested Components
+ * React tools
  * */
 
 
 function Greeting(){
     return (
         <div className=''>
-            <h3>Hello people</h3>
-            <ul>
-                <li>
-                    <a href='#'>hello world</a>
-                </li>
-                <img src='' alt=''/>
-                <input type="text"/>
-            </ul>
+            <Person/>
+            <Message/>
         </div>
     );
+}
+
+const Person = () => <h2>John Doe</h2>;
+const Message = () => {
+    return <p>This is my message</p>;
 }
 
 ReactDOM.render(
