293b9bfa2d352ce71fcd73a98652ea4938460413
abdvkh <e-address@bk.ru>
2021-01-23 11:56:13 +0500

    JSX rules

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/** JSX rules
+ * return single element
+ * div / section / article or Fragment
+ * use camelCase for html attribute
+ * className instead of class
+ * close every element
+ * formatting
+ * */
+
+
 function Greeting(){
     return (
-        <div>
-            <h1>Hello world! This is my first component</h1>
+        <div className=''>
+            <h3>Hello people</h3>
+            <ul>
+                <li>
+                    <a href='#'>hello world</a>
+                </li>
+                <img src='' alt=''/>
+                <input type="text"/>
+            </ul>
         </div>
     );
 }
-
-// const Greeting = () => {
-//     return React.createElement(
-//         'div',
-//         {},
-//         React.createElement(
-//             'h1',
-//             {},
-//             'Hello world')
-//     );
-// }
 
 ReactDOM.render(
     <Greeting/>,
