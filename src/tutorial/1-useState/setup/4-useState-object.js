896592f30b5f66b4178986654216af2b86e80b0a
abdvkh <e-address@bk.ru>
2021-01-24 21:57:18 +0500

    useState hook using an Object;

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'Peter',
+    age: 24,
+    message: 'Random message'
+  });
+
+  const [name, setName] = useState('Peter');
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState('Random message');
+
+  const changeMessage = () => {
+    // setPerson({...person, message: "Hello world"});
+    setMessage('Hello world!');
+  };
+
+  return <>
+    <h3>{name}</h3>
+    <h3>{age}</h3>
+    <h3>{message}</h3>
+    <button className="btn" onClick={changeMessage}>
+      Change message
+    </button>
+  </>;
 };
 
 export default UseStateObject;
