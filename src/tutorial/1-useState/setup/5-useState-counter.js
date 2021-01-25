0643d92eb3d3199cd7fc51e650510c6a7932e2b0
abdvkh <e-address@bk.ru>
2021-01-25 15:20:46 +0500

    useState hook counter;

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,33 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  const increase = () => setValue(value + 1);
+  const decrease = () => setValue(value - 1);
+  const reset = () => setValue(0);
+
+  const complexIncrease = () => {
+    // setTimeout(() => setValue(value + 1), 3000);
+    setTimeout(() => setValue((prevValue) => prevValue + 1), 3000);
+  }
+
+  return <>
+    <section style={{margin: '4rem 0'}}>
+      <h2>regular counter</h2>
+      <h1>{value}</h1>
+      <button className="btn" onClick={decrease}>decrease</button>
+      <button className="btn" onClick={reset}>reset</button>
+      <button className="btn" onClick={increase}>increase</button>
+    </section>
+    <section style={{margin: '4rem 0'}}>
+      <h2>more complex counter</h2>
+      <h1>{value}</h1>
+      <button className="btn" onClick={complexIncrease}>increase later</button>
+      {/*<button className="btn" onClick={decrease}>decrease</button>*/}
+      {/*<button className="btn" onClick={reset}>reset</button>*/}
+    </section>
+  </>;
 };
 
 export default UseStateCounter;
