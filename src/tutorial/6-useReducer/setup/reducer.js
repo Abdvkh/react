c921199d6f017ec1b99b7755855ad245d3388d7a
abdvkh <e-address@bk.ru>
2021-01-27 13:56:59 +0500

    useReducer

---
+++ src/tutorial/6-useReducer/setup/reducer.js
@@ -0,0 +1,38 @@
+export  default(state, action) => {
+    if(action.type === 'ADD_ITEM'){
+        const newPeople = [...state.people, action.payload];
+
+        return {
+            ...state,
+            people: newPeople,
+            isModalOpen: true,
+            modalContent: 'Item added'
+        };
+    }
+
+    if (action.type === 'NO_VALUE'){
+        return {
+            ...state,
+            isModalOpen: true,
+            modalContent: 'please enter value'
+        };
+    }
+
+    if (action.type === 'CLOSE_MODAL'){
+        return {
+            ...state,
+            isModalOpen: false,
+        };
+    }
+
+    if (action.type === 'REMOVE_ITEM'){
+        const newPeople = state.people.filter(({id}) => action.payload !== id);
+
+        return {
+            ...state,
+            people: newPeople
+        };
+    }
+
+    throw new Error('No matching action type');
+}
